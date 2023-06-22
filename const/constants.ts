3900ec058760c41753cdfb0917e596f9ffba7edb
ciaranightingale <ciara.nightingale@sky.com>
2023-06-22 01:13:03 +0100

    Initial commit from npx thirdweb create

---
+++ const/constants.ts
@@ -0,0 +1,13 @@
+import { Mumbai, Chain } from '@thirdweb-dev/chains'
+
+// your token bound factory address
+export const factoryAddress: string = '0x02101dfB77FDE026414827Fdc604ddAF224F0921'
+export const implementation: string = '0x20bA1a66073E0A7aC2f727C85244D1eb34c0ECFE'
+
+// Your thirdweb api key - you can get one at https://thirdweb.com/dashboard/api-keys
+export const TWApiKey: string =
+    '7a023c31cebbd59b3a6879fd40cc931ae97fb7e5764df9402eb3023ccca38821d455956578e40e4af2d5bf0f033462e65fd961a0b10453455eb4e074f5e7e559'
+export const activeChain: Chain = Mumbai
+
+export const nftDropAddress: string = '0xE0EDdB4df871a023c3e72E72185c3ccbF6F2E58F'
+export const tokenAddress: string = '0x006eF094e2DF803074Ef7Bce35994771a87a6fca'
